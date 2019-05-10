8e9b3098d7f86822d18fe6113d742ae0e57d207e
Mark Ulrich <mark.ulrich.86@gmail.com>
2019-05-09 21:53:02 -0500

    Basic styling

--- src/components/panes/Pane.js
+++ src/components/panes/Pane.js
@@ -20,16 +20,49 @@ export class Pane extends Component {
     };
   }
 
+  componentDidUpdate() {
+    if (!this.props.isExpanded) {
+    }
+  }
+
+  /**
+   *  Toggle pane between normal and maximized state
+   */
+  toggleMaximized = () => {
+    const isMaximized = !this.state.isMaximized;
+    this.setState({ isMaximized });
+  };
+
+  /**
+   *  Toggle pane between expanded and contracted state
+   */
+  toggleExpanded = () => {
+    const isExpanded = !this.state.isExpanded;
+    this.setState({ isExpanded });
+  };
+
   render() {
     const id = this.props.name ? `pane-${this.props.name.toLowerCase()}` : '';
     const className = 'pane';
 
+    const contentComponent = this.state.isExpanded ? (
+      <div className='pane-content-component'>
+        {this.props.contentComponent}
+      </div>
+    ) : (
+      <div />
+    );
+
     return (
       <div className={className} id={id}>
-        <TitleBar title={this.props.title} />
-        <div className='pane-content-component'>
-          {this.props.contentComponent}
-        </div>
+        <TitleBar
+          title={this.props.title}
+          isMaximized={this.state.isMaximized}
+          toggleMaximized={this.toggleMaximized}
+          isExpanded={this.state.isExpanded}
+          toggleExpanded={this.toggleExpanded}
+        />
+        {contentComponent}
       </div>
     );
   }
--- src/components/panes/TitleBar.js
+++ src/components/panes/TitleBar.js
@@ -5,15 +5,36 @@ const DefaultClassName = 'pane-titlebar';
 
 export class TitleBar extends Component {
   static propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    isMaximized: PropTypes.bool.isRequired,
+    toggleMaximized: PropTypes.func.isRequired,
+    isExpanded: PropTypes.bool.isRequired,
+    toggleExpanded: PropTypes.func.isRequired
   };
 
   render() {
     const id = `pane-titlebar-${this.props.title}`;
+    const maxIcon = this.props.isMaximized
+      ? 'fa-window-minimize'
+      : 'fa-window-maximize';
+    const expandedIcon = this.props.isExpanded
+      ? 'fa-caret-down'
+      : 'fa-caret-right';
 
     return (
       <div className={DefaultClassName} id={id}>
+        <div className='pane-expand-icon' onClick={this.props.toggleExpanded}>
+          <i className={`pane-titlebar-icon fas ${expandedIcon} fa-2x`} />
+        </div>
         <h2 className='title-bar-title'>{this.props.title}</h2>
+        <div className='pane-titlebar-icon-container'>
+          <div
+            className='pane-titlebar-icon pane-titlebar-icon-minmax'
+            onClick={this.props.toggleMaximized}
+          >
+            <i className={`far ${maxIcon} fa-2x`} />
+          </div>
+        </div>
       </div>
     );
   }
